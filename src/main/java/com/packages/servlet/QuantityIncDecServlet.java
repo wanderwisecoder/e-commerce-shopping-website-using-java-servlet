eda7ad6010cc6a453ccbe6ef2a5607acc80c398e
wanderwisecoder <thissurajneversets@gmail.com>
2023-09-17 16:20:41 +0530

    Increase and Decrease Cart Product Quantity in JSP and Servlet

--- src/main/java/com/packages/servlet/QuantityIncDecServlet.java
+++ src/main/java/com/packages/servlet/QuantityIncDecServlet.java
@@ -1,12 +1,11 @@
 package com.packages.servlet;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.util.*;
+
+import com.packages.models.Cart;
 
 
 public class QuantityIncDecServlet extends HttpServlet {
@@ -14,8 +13,46 @@ public class QuantityIncDecServlet extends HttpServlet {
 
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			
-		PrintWriter out = response.getWriter();		
-		out.println("quantity-in-dec Servlet");
+		try(PrintWriter out = response.getWriter();	)
+		{
+			String action = request.getParameter("action");
+			int id = Integer.parseInt(request.getParameter("id"));
+			
+			ArrayList <Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list");
+			
+			
+			if(action != null & id>=1)
+			{
+				if(action.equals("inc"))
+				{
+					for( Cart c : cart_list )
+					{
+						if ( c.getId() == id && c.getQuantity()>=1 )
+						{
+							int quantity = c.getQuantity();
+							quantity++;
+							c.setQuantity(quantity);
+							
+						}
+					}
+				}
+				else if(action.equals("dec"))
+				{
+					for( Cart c:cart_list)
+					{
+						if ( c.getId() == id && c.getQuantity()>1 )
+						{
+							int quantity = c.getQuantity();
+							quantity--;
+							c.setQuantity(quantity);
+//							response.sendRedirect("cart.jsp");
+						}
+					}
+				}
+				response.sendRedirect("cart.jsp");
+			}
+		}
+		
 		
 	}
 
